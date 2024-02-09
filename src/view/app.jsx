6dcfd1bd4cfdf90897b2f1cf7ddbd61214feb08e
ShuhaibAkthar <akgk75772@gmail.com>
2024-02-09 12:07:14 +0530

    router added

--- src/view/app.jsx
+++ src/view/app.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+import Password from './pages/Password.jsx';
+
+
 
 createRoot(document.getElementById('root')).render(
-    <div>hello akthar</div>
+ <BrowserRouter>
+ <Routes>
+  
+  <Route path="/main_window" element={<Password/>} />
+ </Routes>
+ </BrowserRouter>
+  
 )
